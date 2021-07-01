711f94f689ee80150a10fa8e43692e46c321b1b4
Samana Shrestha <samanashrestha02@gmail.com>
2021-07-01 12:46:28 +0545

    variables

---
+++ MultipleAssignment.py
@@ -0,0 +1,12 @@
+#Different values multiple assignment
+name,age,height="Samana",19,170.68
+print(name)
+print(age)
+print(height)
+
+#same value multiple assignment
+spongebob=patric=sandy=squidward=25
+print(spongebob)
+print(patric)
+print(sandy)
+print(squidward)
---
+++ Variables.py
@@ -0,0 +1,11 @@
+#Types of variables
+#string,integer,float,boolean
+name="Samana"
+age=19
+height=170.688
+lie=False
+print("My name is : " +name +"\n" +"I'm " +str(age )+" years old." + "\n"+ "My height is " +str(height)+ "cm." +"\n"+ "Am i lying? " +str(lie))
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(lie))
