69cb5c332c0958ed602ec93f966cef6d36090d3d
Samana Shrestha <samanashrestha02@gmail.com>
2021-07-05 08:27:13 +0545

    Typecasting

---
+++ TypeCasting.py
@@ -0,0 +1,10 @@
+#typecasting:coverts a data type of value to another data type
+a=23     #int
+b=22.0   #float
+c="34"   #string
+#print(a)
+# print(b)
+# print(c)
+print(int(b))
+print(float(c))
+print(float(a))
