387cd5f60f6928828548304167f250e73c61bfe3
Samana Shrestha <samanashrestha02@gmail.com>
2021-07-05 08:53:49 +0545

    MathFunction

---
+++ MathFunctions.py
@@ -0,0 +1,12 @@
+import math
+hi= 345.261
+a=3
+b=4
+c=1
+print(round(hi))      #rounds up the decimal
+print(math.ceil(hi))  #rounds up decimal to +1 value
+print(math.floor(hi)) #rounds up decimal to -1 value
+print(abs(hi))        #prints the absolute value of number
+print(math.sqrt(25))  #prints the square root of a number
+print(max(a,b,c))     #prints the maximum value among given numbers
+print(min(a,b,c))     #prints the minimum value among given
