8c111da30c59425a81ec1291a91330441cffc1a5
Samana Shrestha <samanashrestha02@gmail.com>
2021-07-06 07:48:35 +0545

    string methods

--- StringMethods.py
+++ StringMethods.py
@@ -1,10 +1,10 @@
 name="SamanaShrestha"
-print(len(name)) #prints length of character
-print(name.find("m")) #finds the position of certain letter
-print(name.capitalize()) #capitalizes first letter of the word
-print(name.upper()) #UPPERCASE
-print(name.lower()) #lowercase
-print(name.isdigit()) #if digits True otherwise False
-print(name.isalpha()) #if alphabet True otherwise False
-print(name.count("a")) #counts the letter in the word
+print(len(name))             #prints length of character
+print(name.find("m"))        #finds the position of certain letter
+print(name.capitalize())     #capitalizes first letter of the word
+print(name.upper())          #UPPERCASE
+print(name.lower())          #lowercase
+print(name.isdigit())        #if digits True otherwise False
+print(name.isalpha())        #if alphabet True otherwise False
+print(name.count("a"))       #counts the letter in the word
 print(name.replace("a","o")) #replaces letters
