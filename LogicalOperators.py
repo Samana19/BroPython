1f25f1c0cf0c603d6aa2f34aa0603c815396462a
Samana Shrestha <samanashrestha02@gmail.com>
2021-07-06 08:55:10 +0545

    logical operators

---
+++ LogicalOperators.py
@@ -0,0 +1,6 @@
+
+temp=float(input("Enter the temperature :"))
+if temp >= 0 and temp <= 30:
+    print("The weather is great today" +"\n"+ "Go out!" )
+elif temp<0 or temp>30:
+    print('The weather is bad today'+"\n"+ "Stay in!")
